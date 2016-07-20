04df09971b06aa87243fd9c31a9f81c46568bd30
Elio Gubser <gubser@users.noreply.github.com>
2016-07-20 10:56:39 +0200

    follow ptocore api changes

--- master.py
+++ master.py
@@ -21,11 +21,13 @@ def grouper(iterable, count):
             break
 
 def main():
-    ac = AnalyzerContext()
+    ac = AnalyzerContext(verbose=True)
 
-    max_action_id, upload_ids = sensitivity.direct(ac.action_set)
+    max_action_id, upload_ids = ac.action_set.direct()
+
     # analyze one upload per run
     upload_ids = [upload_ids[0]] if len(upload_ids) > 0 else []
+
     print("running it with: ", max_action_id, upload_ids)
     ac.set_result_info_direct(max_action_id, upload_ids)
 
@@ -34,13 +36,13 @@ def main():
     filenames = files.map(lambda x: x[0]).collect()
     for filename in filenames:
         metadata, data = files.lookup(filename)[0]
-        upload_id = metadata['_id']
+        upload_action_id = metadata['action_id'][ac.environment]
         merged = dataprep.prepare_data(filename, metadata, data)
 
         for group in grouper(merged.iterrows(), 1000):
             bulk = []
             for _, row in group:
-                obsns = analysis.create_observations(upload_id, row)
+                obsns = analysis.create_observations(upload_action_id, row)
 
                 for obs in obsns:
                     # TODO should be done in validator
